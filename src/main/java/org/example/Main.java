d903924646d37a4bd6a642286d01476bdeb6e419
Luis Angel <angelfgdeveloper@gmail.com>
2023-01-20 14:39:20 -0600

    Uso de consumer para añadir la logica y mandarlo a llamar con sus respectivos datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,12 @@ package org.example;
 import org.example.entities.Movie;
 import org.example.interfaces.OnOneListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -54,6 +57,31 @@ public class Main {
                 .forEach(m -> contentReport.append(m.toString() + "\n")); // concatenar
 
         System.out.println("ContentReport: " + contentReport);
+
+        System.out.println("\n=======Predicate=======");
+        // Obtiene un objeto a evaluar y regresa true o false
+        Predicate<Movie> moviesViewed = movie -> movie.isViewed(); // Verifica si una movie es vista (true)
+        System.out.println("Predicate: " + moviesViewed.test(movies.get(0))); // false => porque movie no esta en vista
+        System.out.println("Predicate: " + moviesViewed.test(movies.get(3))); // true => porque movie ya se vio
+
+        System.out.println("\n=======Consumer=======");
+        StringBuilder stringBuilder1 = new StringBuilder();
+        // Obtiene un objeto como parametro <T> para en seguida añadir la logica y mandarla a otra funcion del tipo
+        // como es el Consumer a un foreach, filter, map, etc.
+        Consumer<Movie> movieEachs = movieEach -> stringBuilder1.append(movieEach + "\n");
+        movies.stream().forEach(movieEachs);
+        System.out.println("StringBuilder: " + stringBuilder1);
+
+        System.out.println("\n=======Consumer Case-uses=======");
+        Consumer<List<Movie>> moviesLogic = moviesArr -> {
+            AtomicInteger atomicInteger1 = new AtomicInteger(1); // Agrega un integer (tipo index) con valor inicial 0
+            List<Movie> moviesArrLogic = moviesArr;
+            moviesArrLogic.stream()
+                    .map(m -> m.getTitle() + " add logic: " + atomicInteger1.getAndIncrement())
+                    .forEach(System.out::println);
+        };
+        moviesLogic.accept(movies);
+
     }
 
     private static List<Movie> addMovies() {
